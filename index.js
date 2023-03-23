87564a9d17ace61a30356fabf431e28f01607294
titus <titus.unavailable@student.moringaschool.com>
2023-03-23 12:54:23 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-function addingEventListener() {
-    const input = document.getElementById('input');
+const button = document.getElementById('button'); 
 
-
-    function clickAlert(){
-        alert('I was clicked!')
-    }
-    input.addEventListener('click', clickAlert);
+function addingEventListener(){
+    
+alert('I was clicked!');
+button.addEventListener('click', addingEventListener);
 }
-addingEventListener(); 
+addingEventListener() 
